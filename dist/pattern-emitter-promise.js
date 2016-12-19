35e8156bbbc8e012b810a197cedb84e973371bb0
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2016-12-19 12:00:40 +0100

    Major 2.0.0 release

--- dist/pattern-emitter-promise.js
+++ dist/pattern-emitter-promise.js
@@ -2,147 +2,148 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
-function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj; }
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-var localPromise = undefined;
+var localPromise = void 0;
 
 try {
-    localPromise = Promise;
-} catch (e) {}
+  localPromise = Promise;
+} catch (e) {
+  // noop
+}
 
 var EventEmitterPrototype = {
-    /**
-     * Emits the event.
-     *
-     * @param  {String} event   event name to emit
-     * @param  {Any}    ...args arguments to pass to event listeners
-     * @return {Promise}        Promise that will be resolved with array of results from all listeners
-     */
+  /**
+   * Emits the event.
+   *
+   * @param  {String} event   event name to emit
+   * @param  {Any}    ...args arguments to pass to event listeners
+   * @return {Promise}        Promise that will be resolved with array of results from all listeners
+   */
+  emit: function emit(event) {
+    var _this = this;
 
-    emit: function emit(event) {
-        var _this = this;
-
-        for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-            args[_key - 1] = arguments[_key];
-        }
-
-        var listeners = this.listeners(event);
-        var listenersResults = [];
-
-        if (!listeners.length) {
-            return localPromise.resolve(listenersResults);
-        }
-
-        return listeners.reduce(function (prevListenerPromise, currentListener) {
-            if (currentListener.once) {
-                _this.removeEventListener(currentListener.event, currentListener.fn);
-            }
-
-            return prevListenerPromise.then(function (prevListenerResult) {
-                listenersResults.push(prevListenerResult);
-
-                var currentListenerResult = currentListener.fn.apply(currentListener, args);
-
-                //this code handles synchronous (not returning Promise) and asynchronous listeners
-                if ((typeof currentListenerResult === 'undefined' ? 'undefined' : _typeof(currentListenerResult)) === 'object' && typeof currentListenerResult.then === 'function') {
-                    return currentListenerResult;
-                } else {
-                    return localPromise.resolve(currentListenerResult);
-                }
-            });
-        }, localPromise.resolve()).then(function (prevListenerResult) {
-            listenersResults.push(prevListenerResult);
-
-            //remove first, because it comes from empty resolved Promise used as initial reduce value
-            listenersResults.shift();
-
-            return listenersResults;
-        });
-    },
-
-    /**
-     * Adds an event listener function.
-     *
-     * @param {String|RegExp}   event       event to listen to. Can be either string or regex. In the latter case listener will be invoked when its regex matches emited event name
-     * @param {Function}        fn          event listener callback
-     * @param {Boolean}         once=false  if `true` listener will be used only once and automatically removed
-     * @return {EventEmitter}   EventEmitter instance, may be used for method chaining
-     */
-    addEventListener: function addEventListener(event, fn, once) {
-        this._listeners.push({
-            fn: fn,
-            once: once,
-            event: event,
-            isRegExp: Object.prototype.toString.call(event) === '[object RegExp]'
-        });
-
-        return this;
-    },
-
-    /**
-     * Same as 'addEventListener' but with `once` flag set to `true`.
-     *
-     * @param  {String|RegExp}  event see `addEventListener`
-     * @param  {Function}       fn    see `addEventListener`
-     * @return {EventEmitter}         see `addEventListener`
-     */
-    once: function once(event, fn) {
-        return this.addEventListener(event, fn, true);
-    },
-
-    /**
-     * Remove event listener. If only event name is provided, all listeners with that name will be removed.
-     * Listeners are tested against event name using strict equality, so to remove event listener added
-     * with pattern, one has to provide referance to the regex object used to define that listener (see example).
-     *
-     * @example
-     * let r = /foo/;
-     *
-     * ee.on(r, () => {});
-     *
-     * ee.removeEventListener(/foo/); //listener not removed
-     * ee.removeEventListener(r); //listener succesfully removed
-     *
-     * @param  {String|RegExp}  event event name or pattern.
-     * @param  {Function}       fn    callback used to define listener
-     * @return {EventEmitter}         EventEmitter instance
-     */
-    removeEventListener: function removeEventListener(event, fn) {
-        this._listeners = this._listeners.filter(function (listener) {
-            return listener.event !== event || fn && listener.fn !== fn;
-        });
-
-        return this;
-    },
-
-    /**
-     * Removes all event listeners by name or pattern (see `removeEventListener`).
-     *
-     * @param  {String|RegExp}  event   event name
-     * @return {EventEmitter}           EventEmitter instance
-     */
-    removeAllListeners: function removeAllListeners(event) {
-        return this.removeEventListener(event);
-    },
-
-    /**
-     * Get listeners associated with event name.
-     *
-     * @param  {String} event   event name
-     * @return {Array}          array of listeners objects with `event`, `fn`, `once` and `isRegExp` properties
-     */
-    listeners: function listeners(event) {
-        return this._listeners.filter(function (listener) {
-            if (listener.isRegExp) {
-                return listener.event.test(event);
-            } else {
-                return listener.event === event;
-            }
-        });
+    for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+      args[_key - 1] = arguments[_key];
     }
+
+    var listeners = this.listeners(event);
+    var listenersResults = [];
+
+    if (!listeners.length) {
+      return localPromise.resolve(listenersResults);
+    }
+
+    return listeners.reduce(function (prevListenerPromise, currentListener) {
+      if (currentListener.once) {
+        _this.removeEventListener(currentListener.event, currentListener.fn);
+      }
+
+      return prevListenerPromise.then(function (prevListenerResult) {
+        listenersResults.push(prevListenerResult);
+
+        var currentListenerResult = currentListener.fn.apply(currentListener, args);
+
+        // this code handles synchronous (not returning Promise) and asynchronous listeners
+        if ((typeof currentListenerResult === 'undefined' ? 'undefined' : _typeof(currentListenerResult)) === 'object' && typeof currentListenerResult.then === 'function') {
+          return currentListenerResult;
+        }
+
+        return localPromise.resolve(currentListenerResult);
+      });
+    }, localPromise.resolve()).then(function (prevListenerResult) {
+      listenersResults.push(prevListenerResult);
+
+      // remove first, because it comes from empty resolved Promise used as initial reduce value
+      listenersResults.shift();
+
+      return listenersResults;
+    });
+  },
+
+  /**
+   * Adds an event listener function.
+   *
+   * @param {String|RegExp}   event       event to listen to. Can be either string or regex. In the latter case listener will be invoked when its regex matches emited event name
+   * @param {Function}        fn          event listener callback
+   * @param {Boolean}         once=false  if `true` listener will be used only once and automatically removed
+   * @return {EventEmitter}   EventEmitter instance, may be used for method chaining
+   */
+  addEventListener: function addEventListener(event, fn, once) {
+    this._listeners.push({
+      fn: fn,
+      once: once,
+      event: event,
+      isRegExp: Object.prototype.toString.call(event) === '[object RegExp]'
+    });
+
+    return this;
+  },
+
+  /**
+   * Same as 'addEventListener' but with `once` flag set to `true`.
+   *
+   * @param  {String|RegExp}  event see `addEventListener`
+   * @param  {Function}       fn    see `addEventListener`
+   * @return {EventEmitter}         see `addEventListener`
+   */
+  once: function once(event, fn) {
+    return this.addEventListener(event, fn, true);
+  },
+
+  /**
+   * Remove event listener. If only event name is provided, all listeners with that name will be removed.
+   * Listeners are tested against event name using strict equality, so to remove event listener added
+   * with pattern, one has to provide referance to the regex object used to define that listener (see example).
+   *
+   * @example
+   * let r = /foo/;
+   *
+   * ee.on(r, () => {});
+   *
+   * ee.removeEventListener(/foo/); //listener not removed
+   * ee.removeEventListener(r); //listener succesfully removed
+   *
+   * @param  {String|RegExp}  event event name or pattern.
+   * @param  {Function}       fn    callback used to define listener
+   * @return {EventEmitter}         EventEmitter instance
+   */
+  removeEventListener: function removeEventListener(event, fn) {
+    this._listeners = this._listeners.filter(function (listener) {
+      return listener.event !== event || fn && listener.fn !== fn;
+    });
+
+    return this;
+  },
+
+  /**
+   * Removes all event listeners by name or pattern (see `removeEventListener`).
+   *
+   * @param  {String|RegExp}  event   event name
+   * @return {EventEmitter}           EventEmitter instance
+   */
+  removeAllListeners: function removeAllListeners(event) {
+    return this.removeEventListener(event);
+  },
+
+  /**
+   * Get listeners associated with event name.
+   *
+   * @param  {String} event   event name
+   * @return {Array}          array of listeners objects with `event`, `fn`, `once` and `isRegExp` properties
+   */
+  listeners: function listeners(event) {
+    return this._listeners.filter(function (listener) {
+      if (listener.isRegExp) {
+        return listener.event.test(event);
+      }
+
+      return listener.event === event;
+    });
+  }
 };
 
 /**
@@ -165,19 +166,17 @@ EventEmitterPrototype.off = EventEmitterPrototype.removeEventListener;
  *
  * @return {Object} event emitter instance
  */
-var EventEmitter = exports.EventEmitter = function EventEmitter() {
-    if (!localPromise || Object.prototype.toString.call(localPromise.resolve()) !== '[object Object]' || Object.prototype.toString.call(localPromise.resolve().then) !== '[object Function]') {
-        console.warn('Promise is not available in this context. You have to either set Promise\n            library with \'setPromiseLibrary\' static method or use runtime with built-in\n            Promise constructor (newest node.js or any modern browser).');
+var EventEmitter = function EventEmitter() {
+  if (!localPromise || Object.prototype.toString.call(localPromise.resolve()) !== '[object Object]' || Object.prototype.toString.call(localPromise.resolve().then) !== '[object Function]') {
+    throw new Error('Promise is not available in this context. You have to either set Promise\n      library with \'setPromiseLibrary\' static method or use runtime with built-in\n      Promise constructor (newest node.js or any modern browser).');
+  }
 
-        return;
-    }
+  var ee = Object.create(EventEmitterPrototype);
 
-    var ee = Object.create(EventEmitterPrototype);
+  // instance own property
+  ee._listeners = [];
 
-    //instance own property
-    ee._listeners = [];
-
-    return ee;
+  return ee;
 };
 
 /**
@@ -197,7 +196,7 @@ var EventEmitter = exports.EventEmitter = function EventEmitter() {
  * @param  {Promise} promiseLibrary Promise constructor
  */
 EventEmitter.setPromiseLibrary = function (promiseLibrary) {
-    localPromise = promiseLibrary;
+  localPromise = promiseLibrary;
 };
 
 /**
@@ -208,6 +207,9 @@ EventEmitter.setPromiseLibrary = function (promiseLibrary) {
  * @return {Boolean}        `true` if `thing` is event emitter from this lib, `false` otherwise
  */
 EventEmitter.isEventEmitter = function (thing) {
-    return (typeof thing === 'undefined' ? 'undefined' : _typeof(thing)) === 'object' && Object.getPrototypeOf(thing) === EventEmitterPrototype;
+  return (typeof thing === 'undefined' ? 'undefined' : _typeof(thing)) === 'object' && Object.getPrototypeOf(thing) === EventEmitterPrototype;
 };
+
+exports.default = EventEmitter;
+exports.EventEmitter = EventEmitter; // for browserify
 },{}]},{},[1]);
